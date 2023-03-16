0c13e14cb21c1d0cfa924c91f78af9ea797d788a
mohamed ibrahim <mohamedkhallaf654@gmail.com>
2023-03-16 16:50:22 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,13 +3,13 @@
 /**
  * main - Entry point
  *
- * Description: A C program that prints with put function 
+ * Description: A C program that prints with put function
  *
- * Return: Alawys 0 (success)
+ * Return: Alawys 0 (Success)
 */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle"):
-	return (0):
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
