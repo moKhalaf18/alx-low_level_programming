9a5b91bd3bba62a59dce73984f9c9c5c0959ee9a
mohamed ibrahim <mohamedkhallaf654@gmail.com>
2023-03-21 18:52:43 -0700

    a

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,1 @@
-#include "main.h"
-/**
-* main - Entry point 
-*Descriptin: 'prints _putcher to stdout'
-* Return: always 0 
-*/
-int main(void)
-{       
-        _putchar('_');       
-        _putchar('p');
-        _putchar('u');       
-        _putchar('t');
-        _putchar('c');       
-        _putchar('h');
-        _putchar('a');       
-        _putchar('z');
-        _putchar('\n');
-        return (0);
 }
