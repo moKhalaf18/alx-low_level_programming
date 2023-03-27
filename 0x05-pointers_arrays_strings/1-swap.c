dcefa189c354bf2183b71e0b741b8adc0ab56232
mohamed ibrahim <mohamedkhallaf654@gmail.com>
2023-03-27 11:06:00 -0700

    a

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of tow integers
+ *		using two in put parameters
+ *
+ *@a: input parameter 1
+ *@b: input parameter 2
+ *
+ * Return: nothing
+*/
+
+void swap_int(int *a,int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
