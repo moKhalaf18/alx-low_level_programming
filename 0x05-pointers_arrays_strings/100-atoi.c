b2010a84cdcf2744f51f4a422e634b13f80b1de2
mohamed ibrahim <mohamedkhallaf654@gmail.com>
2023-03-27 17:32:51 -0700

    a

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _atoi - converts a string to an inrteger
+ * _atoi - converts a string to an integer
  *
  * @s: string input parameter
  *
@@ -14,7 +14,7 @@ int _atoi(char *s)
 	int sign = 1;
 
 	do {
-		if (*s == '_')
+		if (*s == '-')
 			sign *= -1;
 		else if (*s >= '0' && *s <= '9')
 			num = (num * 10) + (*s - '0');
