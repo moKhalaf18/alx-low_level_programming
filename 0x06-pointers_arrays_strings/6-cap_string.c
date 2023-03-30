bd107214cf0963b033ebfb489d810037f4f0f3c3
mohamed ibrahim <mohamedkhallaf654@gmail.com>
2023-03-30 07:30:33 -0700

    a

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * isLower - determines whether ascil is lowercase
+ * @c: character
+ * Return: 1 if true, 0 if false
+*/
+
+int isLower(char c)
+{
+	return (c >= 97 && c<= 122);
+}
+
+/**
+ * isDelimiter - determines whether ascil is a delimiter
+ * @c: character
+ * Retuen: 1 if ture, 0 if false
+*/
+
+int isDelimiter(char c)
+{
+
+	int i;
+	char delimiter[] = " \t\n, .!?\"()[]";
+
+	for (i = 0; i < 12; i++)
+		if (c == delimiter[i])
+			return (1);
+	return (0);
+}
