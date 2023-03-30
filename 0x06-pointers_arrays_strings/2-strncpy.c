052683fe10bf13bc03d88618c1c453eacd91dc68
mohamed ibrahim <mohamedkhallaf654@gmail.com>
2023-03-30 06:46:27 -0700

    a

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _strncpy - A function that copies a string.
+ *
+ * @dest: pointer to destination input buffer
+ * @src: pointer to source input buffer
+ * @n: bytes of @src
+ *
+ * Return: @dest
+*/
+
+char *@strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	/**
+	 * iterate through src array
+	 * where if there is no null byte
+	 * among thr first n bytes of source
+	 * the string placed in dest will not be
+	 * null terminated
+	*/
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	/**
+	 * if the length of soruce is less than n
+	 * whrite additional nullbytes to dest to
+	 * @nsure that a total of n bytes is written
+	*/
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+}
+
